feb44d670526c06424c51b449b82de0912c44f18
Kenth Joel Cardente <70134844+kenthjoelcardente@users.noreply.github.com>
2022-04-27 21:17:51 +0800

    commit message

---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const AuthPage = () => {
+	const navigate = useNavigate();
+	const [name, setName] = useState('');
+
+	const navigateToChatPage = () => {
+		if (name !== '') navigate(`/chat/${name}`);
+	};
+
+	return (
+		<main className='simple-wrapper'>
+			<p className='simple-heading'>Hey there Acer!</p>
+
+			<p id='name-label' className='simple-subhead'>
+				Asian College - Dumaguete Campus
+			</p>
+
+			<div className='simple-section'>
+				<input
+					aria-labelledby='name-label'
+					type='text'
+					autoComplete='name'
+					placeholder='Enter your name here!'
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					onKeyPress={(e) => {
+						if (e.key === 'Enter') navigateToChatPage();
+					}}
+				/>
+			</div>
+
+			<div className='simple-section'>
+				<button onClick={navigateToChatPage}>Open AC - Chat Room</button>
+			</div>
+		</main>
+	);
+};
